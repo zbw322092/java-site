26ad141737670336436fb91878c80e7e5f006d37
Bowen Zhong <bz4e14@soton.ac.uk>
2018-01-21 11:47:48 +0800

    mybatis

---
+++ src/main/java/com/javasite/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.javasite.dao;
+
+import com.javasite.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/javasite/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.javasite.dao;
+
+import com.javasite.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
