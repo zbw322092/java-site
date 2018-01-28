47a61e9889c808866d39b07f3d5bdad81bd52b02
Bowen Zhong <bz4e14@soton.ac.uk>
2018-01-28 16:40:32 +0800

    category module

---
+++ src/main/java/com/javasite/service/ICategoryService.java
@@ -0,0 +1,17 @@
+package com.javasite.service;
+
+import com.javasite.common.ServerResponse;
+import com.javasite.pojo.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse getCategoryAndDeepChildrenCategory(Integer categoryId);
+}
